d6c91f7d681dd97f8b80aad6bdf781ccf0748b0e
VoltG3 <kristians.kodehode@gmail.com>
2022-08-15 14:42:45 +0200

    [ ADDING ] - empty template

---
+++ src/main/java/com/data/biosquare/BiosquareApplication.java
@@ -0,0 +1,13 @@
+package com.data.biosquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiosquareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiosquareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/data/biosquare/BiosquareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.biosquare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiosquareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
