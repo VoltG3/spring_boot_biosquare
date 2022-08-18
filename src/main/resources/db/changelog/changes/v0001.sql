fa2770beb8c6cf1363e72ff6f4a311831f3118dc
VoltG3 <kristians.kodehode@gmail.com>
2022-08-18 10:48:00 +0200

    [ ADDING ] - liquibase migration

---
+++ src/main/resources/db/changelog/changes/v0001.sql
@@ -0,0 +1,10 @@
+CREATE TABLE biosquares (
+    id bigserial NOT NULL,
+    landcode VARCHAR(2),
+    landname VARCHAR(44),
+    region VARCHAR(8),
+    population BIGINT,
+    landarea FLOAT,
+    landmap VARCHAR(39),
+    PRIMARY KEY (id)
+);
